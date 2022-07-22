b7c31215846ca872cbcda5e055056db21e537d5a
Janaina Tavares <tjanaina92@gmail.com>
2022-07-22 01:47:00 -0300

    Alteração no diretório saída e no for

--- alura-Stickers/src/App.java
+++ alura-Stickers/src/App.java
@@ -17,42 +17,53 @@ public class App {
         // 1º cria uma variavel com a URL/Chave/Endereço do que vamos consumir
         String url = "https://alura-imdb-api.herokuapp.com/movies";
         URI endereco = URI.create(url);
-        var client = HttpClient.newHttpClient(); // ali na frente estou usando o var, mas poderia ser também o "HttpClient"
-        var request = HttpRequest.newBuilder(endereco).GET().build(); // ali na frente estou usando o var, mas poderia ser também o "HttpRequest"
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString()); // o BodyHandlers é uma classe vai criar a maneira de ler os dados
+        var client = HttpClient.newHttpClient(); // ali na frente estou usando o var, mas poderia ser também o
+                                                 // "HttpClient"
+        var request = HttpRequest.newBuilder(endereco).GET().build(); // ali na frente estou usando o var, mas poderia
+                                                                      // ser também o "HttpRequest"
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString()); // o BodyHandlers é uma classe
+                                                                                       // vai criar a maneira de ler os
+                                                                                       // dados
         String body = response.body();
 
-        //System.out.println(body); para verificar o txt armazenado no body
+        // System.out.println(body); para verificar o txt armazenado no body
 
         // *extrair só os dados que interessam (tipo, poster e classificação)*
         JsonParser parser = new JsonParser();
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
-        //System.out.println(listaDeFilmes.size()); para verificar o tamanho da lista que deve retornar 250 - ok
-        //System.out.println(listaDeFilmes.get(0)); para verificar o item na posição 0
+        // System.out.println(listaDeFilmes.size()); para verificar o tamanho da lista
+        // que deve retornar 250 - ok
+        // System.out.println(listaDeFilmes.get(0)); para verificar o item na posição 0
 
         // *exibir e manipular os dados*
 
         var geradora = new GeradoraDeFigurinhas();
 
-        //foreach
-        for (Map<String,String> filme : listaDeFilmes) {
-            
-            //a gente pega a URL, título, e estamos abrindo um InputStream, isso é, uma corrente de byte. Cria um gerador de figurinhas
-            String urlImagem = filme.get("image");
+        // foreach
+
+        for (int i = 0; i <= 10; i++) {
+            Map<String, String> filme = listaDeFilmes.get(i);
+
+            // a gente pega a URL, título, e estamos abrindo um InputStream, isso é, uma
+            // corrente de byte. Cria um gerador de figurinhas
+
+            String urlImagem = filme.get("image").replaceAll("(@+)(.*).jpg$", "$1.jpg");           
             String titulo = filme.get("title");
 
             InputStream inputStream = new URL(urlImagem).openStream();
-            var nomeArquivo = titulo + ".png";
+            var nomeArquivo = "saida/"+ titulo + ".png"; //para ir pro diretório de saída, pois antes estava tdo na home
 
             geradora.cria(inputStream, nomeArquivo);
 
             System.out.println(titulo);
             System.out.println();
-            /*System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
-            System.out.println(); */
+            /*
+             * System.out.println(filme.get("title"));
+             * System.out.println(filme.get("image"));
+             * System.out.println(filme.get("imDbRating"));
+             * System.out.println();
+             */
         }
     }
 }
--- alura-Stickers/src/GeradoraDeFigurinhas.java
+++ alura-Stickers/src/GeradoraDeFigurinhas.java
@@ -1,7 +1,5 @@
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStream;
-import java.net.URL;
 import java.awt.image.BufferedImage;
 import java.awt.Graphics2D;
 import java.awt.Color;
@@ -9,41 +7,54 @@ import java.awt.Font;
 import javax.imageio.ImageIO;
 
 public class GeradoraDeFigurinhas {
-    
-    //dentro dessa classe precisamos de um metódo criar(). no fim nosso objetivo é que a saida seja uma imagem-sticker
 
-    public void cria(InputStream inputStream, String nomeArquivo) throws Exception{
+    // dentro dessa classe precisamos de um metódo criar(). no fim nosso objetivo é
+    // que a saida seja uma imagem-sticker
+
+    public void cria(InputStream inputStream, String nomeArquivo) throws Exception {
 
         // *leitura da imagem*
         // InputStream inputStream = new FileInputStream(new File("entrada/filme.jpg"));
-       // InputStream inputStream = new URL("https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@.jpg").openStream();
+        // InputStream inputStream = new
+        // URL("https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@.jpg").openStream();
         BufferedImage imagemOriginal = ImageIO.read(inputStream);
 
         // *criar nova imagem em memória com transparência e com tamanho novo*
         int largura = imagemOriginal.getWidth();
         int altura = imagemOriginal.getHeight();
         int novaAltura = altura + 200;
-        
-        BufferedImage novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT); //vai criar uma nova imagem vazia que o fundo é transparente e estamos colocando dentro do novaImagem
+
+        BufferedImage novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT); // vai criar uma
+                                                                                                      // nova imagem
+                                                                                                      // vazia que o
+                                                                                                      // fundo é
+                                                                                                      // transparente e
+                                                                                                      // estamos
+                                                                                                      // colocando
+                                                                                                      // dentro do
+                                                                                                      // novaImagem
 
         // *copiar a imagem original para nova imagem (em memória)*
-       Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
-       graphics.drawImage(imagemOriginal, 0, 0, null);
+        Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
+        graphics.drawImage(imagemOriginal, 0, 0, null);
 
-       // *configurar a fonte*
-       var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 64);
-       graphics.setColor(Color.MAGENTA);
-       graphics.setFont(fonte);
+        // *configurar a fonte*
+        var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 64);
+        graphics.setColor(Color.YELLOW);
+        graphics.setFont(fonte);
 
         // *escrever uma frase na nova imagem*
-        graphics.drawString("Melhor Filme", 100, novaAltura - 100);
+        graphics.drawString("FILME TOP", 100, novaAltura - 100);
 
-        //escrever a nova imagem em um arquivo
+        // escrever a nova imagem em um arquivo
         ImageIO.write(novaImagem, "png", new File(nomeArquivo));
     }
 
-    /* Esse main estava sendo usado para testes dos InputStream de leitura de imagem que também estão comentados.
-    public static void main(String[] args) throws Exception {
-    var geradora = new GeradoraDeFigurinhas();
-    geradora.cria(); }*/
+    /*
+     * Esse main estava sendo usado para testes dos InputStream de leitura de imagem
+     * que também estão comentados.
+     * public static void main(String[] args) throws Exception {
+     * var geradora = new GeradoraDeFigurinhas();
+     * geradora.cria(); }
+     */
 }
